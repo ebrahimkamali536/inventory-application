48e0e3bd0ecca11825fe92e256863c2c11e972dd
ebrahim kamali <ebrahimkamali536@gmail.com>
2023-03-13 17:34:13 +0330

    initialize react application and install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        base: "#666E7D",
+        primary: "#344293",
+        accent: "#E91868",
+        accentDark: "#D0165D",
+        attention: "#F8B114",
+        blueActive: "#6774BD",
+        blueLight: "#A5ADD7",
+        purple: "#6D5BD0",
+        inactive: "#666E7D",
+        green: "#02A443",
+        visited: "#000000",
+      }
+    },
+  },
+  plugins: [],
+}
